0fa7cdb931ec27b55f774879a66612cfa66d37b1
Marcos Delgado <marcosdelgado0408@gmail.com>
2022-05-26 23:32:04 -0300

    Adicionando arquivos iniciais

---
+++ sistema_bancario/src/main/java/org/example/Menu.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class Menu {
+
+
+    public void showMenu(){
+        System.out.println("=======================================");
+        System.out.println("       Sistema bancario primitivo");
+        System.out.println("Digite a acao que deseja realizar:");
+        System.out.println("1 - Cadastrar conta");
+        System.out.println("2 - Consultar saldo");
+        System.out.println("3 - Solicitar credito");
+        System.out.println("4 - Debitar");
+        System.out.println("5 - Transferencia");
+        System.out.println("=======================================");
+    }
+
+    public void goToMethod(char input){
+        switch (input){
+
+            case '1':
+                
+                break;
+
+            case '2':
+
+
+        }
+
+    }
+
+
+}
